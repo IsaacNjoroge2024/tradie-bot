af60323d6808f0266059208fe7ae316916c95230
Isaac Njoroge <isaacnj02@gmail.com>
2026-02-25 16:07:10 +0300

    ISA-5 => TICKET 1: Project Initialization & Repository Setup (#1)
    feat: ticket-1 - project initialization and repository setup

---
+++ alert-service/src/test/java/com/tradie/alert/AlertServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.tradie.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    properties = {
+        "spring.autoconfigure.exclude=" +
+            "org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration"
+    }
+)
+class AlertServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ strategy-engine/src/main/java/com/tradie/strategy/StrategyEngineApplication.java
@@ -0,0 +1,12 @@
+package com.tradie.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyEngineApplication.class, args);
+    }
+}

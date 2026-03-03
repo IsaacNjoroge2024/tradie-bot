e21f7d5e7d5b699abaa677de536b203ef595ef3d
Isaac Njoroge <isaacnj02@gmail.com>
2026-03-03 16:53:17 +0300

    fixing build

--- api-gateway/src/test/java/com/tradie/gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/tradie/gateway/ApiGatewayApplicationTests.java
@@ -7,16 +7,22 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(
     properties = {
+        // Use H2 in-memory so JpaConfig/@EnableJpaRepositories can initialise without a real DB
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.flyway.enabled=false",
+        // No Kafka broker needed in context load test
         "spring.autoconfigure.exclude=" +
-            "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
             "org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration"
     }
 )
 class ApiGatewayApplicationTests {
 
-    // JPA auto-config is excluded, so JPA repositories are not created.
-    // KafkaConfig provides KafkaTemplate; TradeSignalRepository must be mocked.
+    // JpaConfig/@EnableJpaRepositories scans for TradeSignalRepository; mock it so
+    // no real DB queries are executed during the context load test.
     @MockBean
     TradeSignalRepository tradeSignalRepository;
 
